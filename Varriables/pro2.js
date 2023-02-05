9cbc8f524fa7552280102c2b2ba91cd5ae8f7e9f
rathodswapnil5 <swapnilrathod6004@gmail.com>
2023-02-05 20:45:20 +0530

    first commit

---
+++ Varriables/pro2.js
@@ -0,0 +1,8 @@
+let name= "Swapnil Shivaji Rathod"
+console.log(name)
+
+ name= "Shivaji Lalu Rathod"
+console.log(name)
+
+ name= "Sakubai Shivaji Rathod"
+console.log(name)
---
+++ Varriables/pro4.js
@@ -0,0 +1,34 @@
+ let name = "Swapnil Rathod";
+ let school = "Masai School";
+let Grade = "graduation";
+let section = "A";
+let RollNo= 69;
+let mark= "Subject Marks"
+let Sub1 =  95;
+let Sub2 =    89;
+let Sub3 =   88;
+
+
+console.log("________________________________")
+console.log("|     Name      |"+name+"  |")
+console.log("| _______________________________|")
+console.log("|    School     |  "+school+"  |")
+console.log("| _______________________________|")
+console.log("|    Grade      |"+Grade+"      |")
+console.log("|________________________________|")
+console.log("|   Section     |" +section+"               |")
+console.log("| _______________________________|")
+console.log("|   RollNo      |  "+RollNo+"            |")
+console.log("| _______________________________|") 
+console.log("                                       ")
+console.log("                                       ")
+console.log("        "+mark+"                           ")
+ 
+ 
+console.log(" ________________________________") 
+console.log("| Subject-1    |  "+Sub1+" ")    |
+console.log("| _______________________________|")
+console.log("| subject-2    |  "+Sub2+" ")
+console.log("| _______________________________|")
+console.log("|  Subject-3   |  "+Sub3+" ")
+console.log("|________________________________|")
